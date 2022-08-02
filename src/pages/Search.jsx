6b225c5293be29ce6f9890754ea97c7449ec5f2c
Diego Picoli <dgpicoli@gmail.com>
2022-08-01 21:09:37 -0300

    Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,43 @@ import React, { Component } from 'react';
 import Header from './Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      search: '',
+    };
+  }
+
+  onInputChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { search } = this.state;
+    const minInput = 2;
     return (
       <div data-testid="page-search">
         <Header />
         Search
+        <input
+          data-testid="search-artist-input"
+          type="text"
+          name="search"
+          id=""
+          onChange={ this.onInputChange }
+        />
+        <button
+          data-testid="search-artist-button"
+          disabled={ search.length < minInput }
+          type="submit"
+        >
+          Procurar
+
+        </button>
       </div>
     );
   }
